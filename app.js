3c9ee60f8525e2b71683917bc5025f1b57c90899
Alan Chen <azc242@nyu.edu>
2020-07-20 13:45:19 -0700

    unblock CORS

--- app.js
+++ app.js
@@ -4,6 +4,11 @@ const Quote = require('inspirational-quotes');
 
 const app = express();
 
+app.use(function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
 
 app.get("/", function(req, res) {
   res.send(Quote.getQuote());
