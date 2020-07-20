64cce4db5563bb1d59d7167f8234607504009252
Alan Chen <azc242@nyu.edu>
2020-07-20 12:53:36 -0700

    completed backend setup

---
+++ app.js
@@ -0,0 +1,18 @@
+// jshint esversion: 6
+const express = require("express");
+const Quote = require('inspirational-quotes');
+
+const app = express();
+
+
+app.get("/", function(req, res) {
+  res.send(Quote.getQuote());
+});
+
+let port = process.env.PORT;
+if(port == null || port == "") {
+	port = 3000;
+}
+app.listen(port, function() {
+	console.log("Server started successfully");
+});
